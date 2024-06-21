022987d8d74a90c6325dfd9925aeffd3b3c2fc71
Elizabeth Ray <elizabeth-94@hotmail.co.uk>
2024-06-21 10:09:06 +0100

    adds functionality to allow users to see lists of articles for each topic in a seperate page, using the navbar links

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react"
 import { getArticles } from "../api"
 import ArticleItem from "./ArticleItem"
-
+import { useParams } from "react-router-dom"
 
 const Articles = () => {
+    const { topic } = useParams();
     const [articles, setArticles] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     
     useEffect(() => {
         setIsLoading(true)
-        getArticles().then((articlesFromAPI) => {
+        getArticles(topic).then((articlesFromAPI) => {
             setArticles(articlesFromAPI.data.articles)
             setIsLoading(false)
         })
-    }, [])
+    }, [topic])
 
     if(isLoading){
         return <p className="loading">Loading...</p>
     }
     return (<>
+        <h2 className="articles-subheader">{topic ? `${topic.slice(0, 1).toUpperCase()}${topic.slice(1)} Articles:` : "All Articles:"}</h2>
         <ul className="article-list">
             {articles.map((article) => {
                 return <ArticleItem key={article.article_id} singleArticle={article}/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { getTopics } from "../api"
+
 
 const NavBar = () => {
+    const [topics, setTopics] = useState([])
+
+    useEffect(()=>{
+        getTopics().then((topicsFromApi) => {
+            setTopics(topicsFromApi.data.topics)
+        })
+    }, [])
+
     return (
         <nav className="nav">
-            <Link to="/">Home</Link>
+            <Link to="/" className="nav-item">Home</Link>
+            {topics.map((topic) => <Link to={`/articles/${topic.slug}`} key={topic.slug} className={"nav-item" } >{topic.slug.slice(0, 1).toUpperCase() + topic.slug.slice(1, )}</Link>)}
         </nav>
     )
 }
